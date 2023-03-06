5e8f3326b2dd555eab9735d68a9719227c0b7117
PolanianRifleman <124378806+PolanianRifleman@users.noreply.github.com>
2023-03-06 15:35:41 +0000

    hope

--- program.py
+++ program.py
@@ -1,10 +1,10 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statementprint("Hello World")
 def myFunction():
-  print("Hello World")
-
+  print("Hello World!")
+  
 myFunction()
 
 name = input("What is your name ")
 
-print("Hello",name)
+print("Hello",name,"!")
